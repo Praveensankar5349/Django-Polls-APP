bb6e2e3ddbe65223f93ed078ae6ab558320262d8
Praveensankar5349 <praveensankarv98@gmail.com>
2023-08-03 21:08:00 +0530

    modification in urls.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     # ex: /polls/5/results/
     path('polls/<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
+    
     path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
